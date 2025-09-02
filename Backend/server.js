86199e7ea521d1950bc7d18f04e407346829094e
pkpotter03 <pkpotter03@gmail.com>
2025-09-02 11:31:46 +0530

    added cors

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,10 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import cors from "cors";
 
 const app = express();
+app.use(cors()); 
 const PORT = 5000;
 
 // Resolve __dirname in ES Modules
